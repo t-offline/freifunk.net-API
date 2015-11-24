a3bdaddf6641c8570ae7fd1bca520851642d8eb7
Andreas Ziegler <github@andreas-ziegler.de>
2015-11-24 05:50:51 +0100

    more information in API and filename change

--- freifunkAPIupdater.py
+++ freifunkAPIupdater.py
@@ -5,7 +5,7 @@ from json import dumps
 import re
 from datetime import datetime
 
-json_data = open('freifunk.net.json')
+json_data = open('altdorf.freifunk.net.json')
 data = json.load(json_data)
 json_data.close()
 
@@ -22,7 +22,7 @@ print data["state"]["lastchange"]
 print data["state"]["nodes"]
 
 try:
-  with open('freifunk.net.json', 'w') as fn:
+  with open('altdorf.freifunk.net.json', 'w') as fn:
     fn.write(dumps(data, indent=4))
     fn.close()
 except Exception as ex:
--- updateFreifunkAPI.sh
+++ updateFreifunkAPI.sh
@@ -3,6 +3,6 @@
 git pull
 python freifunkAPIupdater.py
 
-git add freifunk.net.json
+git add altdorf.freifunk.net.json
 git commit -a -m "updated number of nodes"
 git push git@github.com:tecff/freifunk.net-API.git
