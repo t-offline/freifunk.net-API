395f19c34d9776c6a12617ddc744c7409b05b3a5
Andreas Ziegler <github@andreas-ziegler.de>
2015-11-24 05:51:03 +0100

    add landshut.freifunk.net.json

--- updateFreifunkAPI.sh
+++ updateFreifunkAPI.sh
@@ -4,5 +4,6 @@ git pull
 python freifunkAPIupdater.py
 
 git add altdorf.freifunk.net.json
+git add landshut.freifunk.net.json
 git commit -a -m "updated number of nodes"
 git push git@github.com:tecff/freifunk.net-API.git
