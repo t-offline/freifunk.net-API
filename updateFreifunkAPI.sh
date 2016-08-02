73055e9d85f942c3d3b1328b58a3e4af7dc36511
Andreas Ziegler <github@andreas-ziegler.de>
2016-08-02 17:21:00 +0200

    bugfixes for updateFreifunkAPI.sh

--- updateFreifunkAPI.sh
+++ updateFreifunkAPI.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 git pull
+
+python node_stats.py > node_stats
 python freifunkAPIupdater.py
+
+for i in altdorf landshut; do
+	sed -i "s| $||" ${i}.freifunk.net.json
+done
 
 git add altdorf.freifunk.net.json
 git add landshut.freifunk.net.json
-git commit -a -m "updated number of nodes"
+git commit -m "updated number of nodes"
 git push git@github.com:tecff/freifunk.net-API.git
+git fetch origin
