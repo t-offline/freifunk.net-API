16b0f4674f747879123f17c5d6d1ac3895cc6c14
Andreas Ziegler <github@andreas-ziegler.de>
2016-08-02 17:20:38 +0200

    add landshut handling to freifunkAPIupdater.py

--- freifunkAPIupdater.py
+++ freifunkAPIupdater.py
@@ -5,26 +5,29 @@ from json import dumps
 import re
 from datetime import datetime
 
-json_data = open('altdorf.freifunk.net.json')
-data = json.load(json_data)
-json_data.close()
+jsonfiles = [ 'altdorf.freifunk.net.json', 'landshut.freifunk.net.json' ]
+for index, jsonfile in enumerate(jsonfiles):
+  print jsonfile
+  json_data = open(jsonfile)
+  data = json.load(json_data)
+  json_data.close()
 
-try:
-  with open('node_stats', 'r') as fn:
-    numbers = re.findall(r'\d+', fn.readline())
-    nodes = numbers[2]
-except Exception as ex:
-  exit(ex)
+  try:
+    with open('node_stats', 'r') as fn:
+      numbers = re.findall(r'\d+', fn.readline())
+      nodes = numbers[2]
+  except Exception as ex:
+    exit(ex)
 
-data["state"]["lastchange"] = datetime.now().isoformat('T')
-data["state"]["nodes"] = int(nodes)
-print data["state"]["lastchange"]
-print data["state"]["nodes"]
+  data["state"]["lastchange"] = datetime.now().isoformat('T')
+  data["state"]["nodes"] = int(nodes)
+  print data["state"]["lastchange"]
+  print data["state"]["nodes"]
 
-try:
-  with open('altdorf.freifunk.net.json', 'w') as fn:
-    fn.write(dumps(data, indent=4))
-    fn.close()
-except Exception as ex:
-  exit(ex)
+  try:
+    with open(jsonfile, 'w') as fn:
+      fn.write(dumps(data, indent=4))
+      fn.close()
+  except Exception as ex:
+    exit(ex)
 
